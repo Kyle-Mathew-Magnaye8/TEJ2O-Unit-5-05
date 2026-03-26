1094c13295ace7f0c8a5f5916f3630f0fd9b629a
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-03-26 08:28:49 -0400

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Mar 2026
+ * This program will tell the distance in cm to the sonar.
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// find the distance from sonar
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+  )
+  basic.showNumber(distanceToObject)
+  basic.showIcon(IconNames.Happy)
+})
