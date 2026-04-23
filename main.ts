b40e3afe3f29b27b2424a83025f1b73a98ed26be
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-04-23 13:08:53 -0400

    changed code

--- main.ts
+++ main.ts
@@ -5,26 +5,25 @@
  * This program will tell the distance in cm to the sonar.
 */
 
+// variables
 let distanceToObject: number = 0
 
-// Setup: Show a happy face to indicate the program is ready
+// setup: Show a happy face to indicate the program is ready
 basic.showIcon(IconNames.Happy)
 
-// Find the distance from sonar when Button A is pressed
+// find the distance from sonar when Button A is pressed
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 
-    // Measure distance
+    // measures distance
     distanceToObject = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
     )
 
-    // Display the result
+    // display the result
     basic.showNumber(distanceToObject)
-
-    // Brief pause so the user can read the last digit before the icon returns
     basic.pause(500)
     basic.showIcon(IconNames.Happy)
 })
