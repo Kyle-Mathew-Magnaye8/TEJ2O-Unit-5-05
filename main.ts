c46f61189c21b525e7f2736429c8e98bb033f036
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-04-23 13:01:21 -0400

    changed code

--- main.ts
+++ main.ts
@@ -7,17 +7,24 @@
 
 let distanceToObject: number = 0
 
-// setup
+// Setup: Show a happy face to indicate the program is ready
 basic.showIcon(IconNames.Happy)
 
-// find the distance from sonar
+// Find the distance from sonar when Button A is pressed
 input.onButtonPressed(Button.A, function () {
-  basic.clearScreen()
-  distanceToObject = sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-  )
-  basic.showNumber(distanceToObject)
-  basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+
+    // Measure distance
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // Display the result
+    basic.showNumber(distanceToObject)
+
+    // Brief pause so the user can read the last digit before the icon returns
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
 })
